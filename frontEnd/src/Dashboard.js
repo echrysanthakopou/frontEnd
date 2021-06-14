04be6295d598c35365c80e37a68d6f029311016e
echrysanthakopou <echrysanthakopou@gmail.com>
2021-06-14 20:58:20 +0300

    --Modified sign in page

--- frontEnd/src/Dashboard.js
+++ frontEnd/src/Dashboard.js
@@ -1,24 +1,36 @@
 import React from 'react';
 import clsx from 'clsx';
-import {makeStyles} from '@material-ui/core/styles';
+import { makeStyles } from '@material-ui/core/styles';
 import CssBaseline from '@material-ui/core/CssBaseline';
 import Drawer from '@material-ui/core/Drawer';
+import Box from '@material-ui/core/Box';
 import AppBar from '@material-ui/core/AppBar';
 import Toolbar from '@material-ui/core/Toolbar';
 import List from '@material-ui/core/List';
 import Typography from '@material-ui/core/Typography';
 import Divider from '@material-ui/core/Divider';
 import IconButton from '@material-ui/core/IconButton';
+import Badge from '@material-ui/core/Badge';
 import Container from '@material-ui/core/Container';
+import Grid from '@material-ui/core/Grid';
+import Paper from '@material-ui/core/Paper';
 import Link from '@material-ui/core/Link';
 import MenuIcon from '@material-ui/icons/Menu';
 import ChevronLeftIcon from '@material-ui/icons/ChevronLeft';
-import {secondaryListItems, sideMenuHomePage} from './listItems';
-import UpdateCommunicationDetails from './UpdateCommunicationDetails';
-
-import PostsCreate from "./createNewAppl";
+import NotificationsIcon from '@material-ui/icons/Notifications';
+import { sideMenuHomePage, secondaryListItems } from './listItems';
+import ListOfIssues from './ListOfIssues';
+import ReactDOM from "react-dom";
+import EnhancedTable from "./tableArray";
+import PostsCreate from "./postCreate";
 import Search from "./Search"
-import {BrowserRouter, Route, Switch} from 'react-router-dom'
+import { Switch, Route } from 'react-router-dom'
+import { BrowserRouter } from 'react-router-dom'
+import SignInSide from "./SignInSide";
+import createHistory from 'history/createBrowserHistory'
+import UpdatePage from "./UpdatePage";
+
+
 
 
 
@@ -119,7 +131,7 @@ const useStyles = makeStyles(theme => ({
 export default function Dashboard(pros) {
     // const {Provider, Consumer} = React.createContext();
 
-    const [searchData, setSearchData] = React.useState(null);
+    const [searchData,setSearchData]=React.useState(null);
     const classes = useStyles();
     const [open, setOpen] = React.useState(true);
     const handleDrawerOpen = () => {
@@ -135,112 +147,111 @@ export default function Dashboard(pros) {
     return (
 
 
+
         <BrowserRouter>
 
-            <div className={classes.root}>
-                <CssBaseline/>
-                <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
-                    <Toolbar className={classes.toolbar}>
-                        <IconButton
-                            edge="start"
-                            color="inherit"
-                            aria-label="open drawer"
-                            onClick={handleDrawerOpen}
-                            className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
-                        >
-                            <MenuIcon/>
-                        </IconButton>
-                        <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
-                            Δημιουργία Φοιτητών
-                        </Typography>
-                    </Toolbar>
-                </AppBar>
-                <Drawer
-                    variant="permanent"
-                    classes={{
-                        paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
-                    }}
-                    open={open}
-                >
-                    <div className={classes.toolbarIcon}>
-                        <IconButton onClick={handleDrawerClose}>
-                            <ChevronLeftIcon/>
-                        </IconButton>
-                    </div>
-                    <Divider/>
-                    <List>{sideMenuHomePage}</List>
-                    <Divider/>
-                    <List>{secondaryListItems}</List>
-                </Drawer>
+        <div className={classes.root}>
+            <CssBaseline />
+            <AppBar position="absolute" className={clsx(classes.appBar, open && classes.appBarShift)}>
+                <Toolbar className={classes.toolbar}>
+                    <IconButton
+                        edge="start"
+                        color="inherit"
+                        aria-label="open drawer"
+                        onClick={handleDrawerOpen}
+                        className={clsx(classes.menuButton, open && classes.menuButtonHidden)}
+                    >
+                        <MenuIcon />
+                    </IconButton>
+                    <Typography component="h1" variant="h6" color="inherit" noWrap className={classes.title}>
+                        Υπηρεσίες Κοινωνικών Ασφαλίσεων
+                    </Typography>
+                </Toolbar>
+            </AppBar>
+            <Drawer
+                variant="permanent"
+                classes={{
+                    paper: clsx(classes.drawerPaper, !open && classes.drawerPaperClose),
+                }}
+                open={open}
+            >
+                <div className={classes.toolbarIcon}>
+                    <IconButton onClick={handleDrawerClose}>
+                        <ChevronLeftIcon />
+                    </IconButton>
+                </div>
+                <Divider />
+                <List>{sideMenuHomePage}</List>
+                <Divider />
+                <List>{secondaryListItems}</List>
+            </Drawer>
 
 
-                <main className={classes.content}>
-                    <div className={classes.appBarSpacer}/>
-                    <Container maxWidth="lg" className={classes.container}>
-                        {/*<Grid container spacing={3}>*/}
+
+
+            <main className={classes.content}>
+                <div className={classes.appBarSpacer} />
+                <Container maxWidth="lg" className={classes.container}>
+                    {/*<Grid container spacing={3}>*/}
                         {/* Chart */}
                         {/*<Grid item xs={500} >*/}
-                        {/*<Paper >*/}
-                        <card>
-                            <Switch>
-                                {/*<Route exact path='/' component={Home}/>*/}
-                                <Route path='/schedule' render={(props) => <Search {...pros} isAuthed={true}/>}/>//
-                                component={Search}/>
-                                <Route path='/create' render={(props) => <PostsCreate {...pros}
-                                                                                      isAuthed={true}/>}/> //component={PostsCreate}/>
-                                <Route path='/listIssues' render={(props) => <UpdateCommunicationDetails {...pros}
-                                                                                                         isAuthed={true}/>}/> //component={PostsCreate}/>
-                                <Route path='/home' render={(props) => <UpdateCommunicationDetails {...pros}
-                                                                                                   isAuthed={true}/>}/> //component={PostsCreate}/>
-                                {/*<Route path='/logout' render={() => { console.log("move back"+window.history); window.history.go(-1);<SignInSide {...pros} isAuthed={true} />}} /> //component={PostsCreate}/>*/}
-                                <Route path='/' render={(props) => <UpdateCommunicationDetails {...pros}
-                                                                                               isAuthed={true}/>}/> //component={PostsCreate}/>
+                            {/*<Paper >*/}
+                            <card>
+                                <Switch>
+                                    {/*<Route exact path='/' component={Home}/>*/}
+                                    <Route path='/schedule' render={(props) => <Search {...pros} isAuthed={true} />} />// component={Search}/>
+                                    <Route path='/create'  render={(props) => <PostsCreate {...pros} isAuthed={true} />} /> //component={PostsCreate}/>
+                                    <Route path='/listIssues'  render={(props) => <ListOfIssues {...pros} isAuthed={true} />} /> //component={PostsCreate}/>
+                                    <Route path='/home' render={(props) => <ListOfIssues {...pros} isAuthed={true} />} /> //component={PostsCreate}/>
+                                    <Route path='/update/:issueID/:name'  component={UpdatePage}   />} /> //component={PostsCreate}/>
+                                    {/*<Route path='/logout' render={() => { console.log("move back"+window.history); window.history.go(-1);<SignInSide {...pros} isAuthed={true} />}} /> //component={PostsCreate}/>*/}
+                                    <Route path='/' render={(props) => <ListOfIssues {...pros} isAuthed={true} />} /> //component={PostsCreate}/>
 
 
-                                {/*<Route path='/logout' component={()=>{*/}
-                                {/*    const history = createHistory();*/}
-                                {/*    history.go(0)}}/>*/}
-                            </Switch>
-                        </card>
-                        {/*</Paper>*/}
+                                    {/*<Route path='/logout' component={()=>{*/}
+                                    {/*    const history = createHistory();*/}
+                                    {/*    history.go(0)}}/>*/}
+                                </Switch>
+                            </card>
+                            {/*</Paper>*/}
                         {/*</Grid>*/}
 
-                        {/*</Grid>*/}
-                        {/* Recent Deposits */}
+                    {/*</Grid>*/}
+                    {/* Recent Deposits */}
 
-                    </Container>
-                </main>
+                </Container>
+            </main>
 
-                {/*<main className={classes.content}>*/}
-                {/*    <div className={classes.appBarSpacer} />*/}
-                {/*    <Container maxWidth="lg" className={classes.container}>*/}
-                {/*        <Grid container spacing={3}>*/}
-                {/*            /!* Chart *!/*/}
-                {/*            <Grid item xs={12} md={8} lg={9}>*/}
-                {/*                <Paper className={fixedHeightPaper}>*/}
-                {/*                   <Search/>*/}
+            {/*<main className={classes.content}>*/}
+            {/*    <div className={classes.appBarSpacer} />*/}
+            {/*    <Container maxWidth="lg" className={classes.container}>*/}
+            {/*        <Grid container spacing={3}>*/}
+            {/*            /!* Chart *!/*/}
+            {/*            <Grid item xs={12} md={8} lg={9}>*/}
+            {/*                <Paper className={fixedHeightPaper}>*/}
+            {/*                   <Search/>*/}
 
-                {/*                </Paper>*/}
-                {/*            </Grid>*/}
-                {/*            /!* Recent Deposits *!/*/}
-                {/*            <Grid item xs={12} md={4} lg={3}>*/}
-                {/*                <Paper className={fixedHeightPaper}>*/}
-                {/*                     <PostsCreate />*/}
-                {/*                </Paper>*/}
-                {/*            </Grid>*/}
-                {/*            /!* Recent Orders *!/*/}
-                {/*            <Grid item xs={12}>*/}
-                {/*                <Paper className={classes.paper}>*/}
-                {/*                    <ListOfIssues />*/}
-                {/*                </Paper>*/}
-                {/*            </Grid>*/}
-                {/*        </Grid>*/}
-                {/*        <Box pt={4}>*/}
-                {/*            <Copyright />*/}
-                {/*        </Box>*/}
-                {/*    </Container>*/}
-                {/*</main>*/}
-            </div>
+            {/*                </Paper>*/}
+            {/*            </Grid>*/}
+            {/*            /!* Recent Deposits *!/*/}
+            {/*            <Grid item xs={12} md={4} lg={3}>*/}
+            {/*                <Paper className={fixedHeightPaper}>*/}
+            {/*                     <PostsCreate />*/}
+            {/*                </Paper>*/}
+            {/*            </Grid>*/}
+            {/*            /!* Recent Orders *!/*/}
+            {/*            <Grid item xs={12}>*/}
+            {/*                <Paper className={classes.paper}>*/}
+            {/*                    <ListOfIssues />*/}
+            {/*                </Paper>*/}
+            {/*            </Grid>*/}
+            {/*        </Grid>*/}
+            {/*        <Box pt={4}>*/}
+            {/*            <Copyright />*/}
+            {/*        </Box>*/}
+            {/*    </Container>*/}
+            {/*</main>*/}
+        </div>
 
         </BrowserRouter>
     );
