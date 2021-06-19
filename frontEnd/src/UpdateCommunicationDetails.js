2aec60ac58c4f16f48f877724ca4e65362026b1b
echrysanthakopou <echrysanthakopou@gmail.com>
2021-06-19 21:07:20 +0300

    Refactor 2

--- frontEnd/src/UpdateCommunicationDetails.js
+++ frontEnd/src/UpdateCommunicationDetails.js
@@ -1,12 +1,6 @@
 import React, {useState} from 'react';
 import {makeStyles} from '@material-ui/core/styles';
 import axios from "axios";
-import Button from "@material-ui/core/Button";
-import Dialog from "@material-ui/core/Dialog";
-import DialogTitle from "@material-ui/core/DialogTitle";
-import DialogContent from "@material-ui/core/DialogContent";
-import DialogContentText from "@material-ui/core/DialogContentText";
-import DialogActions from "@material-ui/core/DialogActions";
 
 import useForm from 'react-hook-form';
 import Title from "./Title";
